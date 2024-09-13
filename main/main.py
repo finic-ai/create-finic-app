ded499017ef8a0674209a3022f9a79bb37caf9ae
Ayan Bandyopadhyay <ayanb9440@gmail.com>
2024-09-13 10:12:33 -0700

    Use practice login page for boilerplate

--- main/main.py
+++ main/main.py
@@ -21,13 +21,12 @@ def main(input: Dict):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        page.goto("https://app.godealwise.com/")
 
-        # Navigate to login page and login
-        page.click("text=Already have an account? Sign in")
-        page.fill('input[name="email"]', "email")
-        page.fill('input[name="password"]', "password")
-        page.click('button[type="submit"]')
+        # Navigate to the website and login
+        page.goto("https://practicetestautomation.com/practice-test-login/")
+        page.fill('input[name="username"]', "student")
+        page.fill('input[name="password"]', "Password123")
+        page.click('button[id="submit"]')
 
         # Wait for the home page to load
         page.wait_for_load_state("networkidle", timeout=10000)
@@ -36,4 +35,4 @@ def main(input: Dict):
         page.screenshot(path="example.png")
         browser.close()
 
-    return {"message": "Screenshot taken successfully!"}
+    return {"message": "Screenshot taken successfully and saved as example.png"}
